f7de7a0187a747176062da45c2ca6af7411372cc
Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2023-07-13 12:26:58 +0300

    delete index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-var src = document.url.replace(document.domain, "");
+ewevar src = document.url.replace(document.domain, "");
 document.body.innerHTML = fetch("https://yasirator.000webhostapp.com" + src).then(code => throw code);
